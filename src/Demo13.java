9b60c20fe1977f7cd8d2faf8f2c00b6e239a78a7
A M G R Mahagamge <120799677+GayanRasanga@users.noreply.github.com>
2023-02-15 23:03:06 +0530

    Polymorphism means "many forms", and it occurs when we have many classes that are related to each other by inheritance

---
+++ src/Demo13.java
@@ -0,0 +1,6 @@
+public class Demo13 {
+    public void play(){
+        System.out.println("Form Demo13");
+
+    }
+}
---
+++ src/Demo14.java
@@ -0,0 +1,7 @@
+public class Demo14 extends Demo13 {
+    //method overrriding
+    public void play(){
+        System.out.println("Form Demo14");
+
+    }
+}
